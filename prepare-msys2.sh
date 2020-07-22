a3e2e0bee0b51cc5ef28ad0020a8b31208bd1fd0
John Murray <MenaceInc@users.noreply.github.com>
2020-07-22 23:50:28 +0100

    Update prepare-msys2.sh to fix dl linker error

--- prepare-msys2.sh
+++ prepare-msys2.sh
@@ -6,4 +6,4 @@ pacman -Syy --noconfirm \
   mingw-w64-i686-zlib mingw-w64-i686-curl mingw-w64-i686-gpgme git \
   subversion patch unzip autoconf automake mingw-w64-i686-cmake \
   mingw-w64-i686-make make mingw-w64-i686-gcc bison flex texinfo \
-  mingw-w64-i686-doxygen libtool
+  mingw-w64-i686-doxygen libtool mingw-w64-i686-dlfcn
